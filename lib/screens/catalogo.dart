45b1b302eec26821329e4ed7938afbca4a490fa9
edocecchini01 <S1101827@studenti.univpm.it>
2023-07-09 16:40:31 +0200

    visualizza i libri nelle liste

--- lib/screens/catalogo.dart
+++ lib/screens/catalogo.dart
@@ -1,11 +1,23 @@
 
+import 'dart:developer';
+
 import 'package:book_tique/screens/bookImage.dart';
 import 'package:book_tique/screens/catalogoGeneri.dart';
 import 'package:book_tique/screens/impostazioni.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:book_tique/models/book.dart';
 
 class CatalogoPage extends StatelessWidget {
+  List<Book> listaDaLeggere = [];
+  List<Book> listaInCorso = [];
+  List<Book> listaLetti = [];
+
+  CatalogoPage() {
+    fillBooks();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,9 +41,9 @@ class CatalogoPage extends StatelessWidget {
                 Navigator.pushNamed(context, '/login');
               },
               icon: Icon(
-                Icons.account_circle,
-                size: 32,
-                color: Color(0xFFB46060)
+                  Icons.account_circle,
+                  size: 32,
+                  color: Color(0xFFB46060)
               ),
             ),
           ],
@@ -46,7 +58,6 @@ class CatalogoPage extends StatelessWidget {
               SizedBox(height: 28),
               GestureDetector(
                 onTap: () {
-                  List<Book> listaDaLeggere = [];
                   _navigateToCatalogoGeneri(context, listaDaLeggere);
                 },
                 child: Text(
@@ -76,7 +87,6 @@ class CatalogoPage extends StatelessWidget {
               SizedBox(height: 28),
               GestureDetector(
                 onTap: () {
-                  List<Book> listaInCorso = [];
                   _navigateToCatalogoGeneri(context, listaInCorso);
                 },
                 child: Text(
@@ -106,7 +116,6 @@ class CatalogoPage extends StatelessWidget {
               SizedBox(height: 28),
               GestureDetector(
                 onTap: () {
-                  List<Book> listaLetti = [];
                   _navigateToCatalogoGeneri(context, listaLetti);
                 },
                 child: Text(
@@ -139,11 +148,75 @@ class CatalogoPage extends StatelessWidget {
       ),
     );
   }
+
   void _navigateToCatalogoGeneri(BuildContext context, List<Book> books) {
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => CatalogoGeneri( books: books)),
+      MaterialPageRoute(builder: (context) => CatalogoGeneri(books: books)),
     );
+  }
+
+  void fillBooks() async {
+    final currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser != null) {
+      final userId = currentUser.uid;
+      final database = FirebaseDatabase(
+          databaseURL: 'https://booktiqueflut-default-rtdb.europe-west1.firebasedatabase.app/');
+      FirebaseDatabase.instance.setPersistenceEnabled(true);
+      FirebaseDatabase.instance.ref().keepSynced(true);
+      DatabaseReference usersRef = database.ref().child("Utenti");
+      DatabaseReference userRef = usersRef.child(userId);
+      DatabaseReference catalogoRef = userRef.child('Catalogo');
+
+      catalogoRef.onValue.listen((DatabaseEvent event) {
+        final data = event.snapshot.value as Map<dynamic, dynamic>;
+        log("data: $data");
+        listaDaLeggere.clear();
+        listaInCorso.clear();
+        listaLetti.clear();
+        if (data != null) {
+          data.forEach((key, value) {
+            // Access the identifier (book ID) of each object
+            log("Book ID: $key");
+            // Access the object data using 'value' variable
+            log("Object Data: $value");
+            if (value['stato'] == "Da leggere") {
+              Book book = Book(
+                id: key,
+                title: value['titolo'],
+                authors: List<String>.from(value['autori']),
+                thumbnailUrl: value['copertina'],
+                description: value['descrizione'],
+              );
+              listaDaLeggere.add(book);
+            }
+            if (value['stato'] == "In corso") {
+              Book book = Book(
+                id: key,
+                title: value['titolo'],
+                authors: List<String>.from(value['autori']),
+                thumbnailUrl: value['copertina'],
+                description: value['descrizione'],
+              );
+              listaInCorso.add(book);
+            }
+            if (value['stato'] == "Letti") {
+              Book book = Book(
+                id: key,
+                title: value['titolo'],
+                authors: List<String>.from(value['autori']),
+                thumbnailUrl: value['copertina'],
+                description: value['descrizione'],
+              );
+              listaLetti.add(book);
+            }
+          });
+        }
+      });
+    } else {
+      // L'utente non è loggato
+      // Esegui qui le azioni necessarie (es. visualizza un messaggio di errore)
+    }
   }
 }
 
